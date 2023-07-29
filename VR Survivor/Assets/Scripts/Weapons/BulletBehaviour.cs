3f96839c661b95eab50e4ee5ef570748cb52dd3a
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2023-07-29 16:19:06 +0200

    shooting

--- VR Survivor/Assets/Scripts/Weapons/BulletBehaviour.cs
+++ VR Survivor/Assets/Scripts/Weapons/BulletBehaviour.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     [SerializeField] float speed;
-    // Update is called once per frame
+    
     void Update()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider col) 
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            Debug.Log("hit");
+            Destroy(col.gameObject);
+        }
+    
     }
 }
