654ef1d38030a6601487cc0650490c3c3be19f6b
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2023-08-01 21:39:18 +0200

    Shooting

--- VR Survivor/Assets/Scripts/Weapons/GunBehaviour.cs
+++ VR Survivor/Assets/Scripts/Weapons/GunBehaviour.cs
@@ -4,21 +4,61 @@ using UnityEngine;
 
 public class GunBehaviour : MonoBehaviour
 {
-
-
     [SerializeField] GameObject bullet;
     [SerializeField] float shootingSpeed;
+    [SerializeField] List<GameObject> bulletList = new List<GameObject>();
+    [SerializeField] int numberOfBullets;
+    [SerializeField] Transform parent;
+    PlayerControls controlls;
+    [SerializeField] string hand;
+
     private void Awake() 
     {
+        controlls = new PlayerControls();
+        SpawningBullets();
         StartCoroutine(Shooting());
+    }
+
+
+    private void SpawningBullets()
+    {
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            var obj = Instantiate(bullet, parent);
+            obj.SetActive(false);
+            bulletList.Add(obj);
+        }
+    }
+
+    private GameObject Pooling()
+    {
+        for (int i = 0; i < bulletList.Count; i++)
+        {
+            if(!bulletList[i].activeInHierarchy)
+            return bulletList[i];
+        }
+        return null;
     }
 
     IEnumerator Shooting()
     {
         while(true)
         {
-            Instantiate(bullet, transform.position, transform.rotation);
-            yield return new WaitForSeconds(shootingSpeed);
+            if((controlls.Shooting.Left.IsPressed() && hand == "Left") || (controlls.Shooting.Right.IsPressed() && hand == "Right"))
+            {
+                var obj = Pooling();
+                obj.SetActive(true);
+                obj.transform.position = transform.position;
+                obj.transform.rotation = transform.rotation;
+
+                yield return new WaitForSeconds(shootingSpeed);
+            }
+
+            yield return null;
         }
     }
+
+
+    private void OnEnable() => controlls.Enable();
+    private void OnDisable() => controlls.Disable();
 }
